e02b828c8ff4a64da75aae6e60b8199870ed2e73
blazebot7000 <cgraf@uvm.edu>
2014-12-03 00:30:48 -0500

    comments on the Card class
    forgot these on the previous commit

--- Card.java
+++ Card.java
@@ -11,23 +11,40 @@ public class Card
    public final int QUEEN = 12;
    public final int KING = 13;
    public final int ACE = 14;
+   /*
+   *@param  String j String with the location of a jpg image
+   *@param  int v    integer value of the card
+   */
    public Card(String j, int v)  
    {
       jpeg = j;
       value = v;
    }
+   /*
+   *@param  String j String with the location of a jpg image
+   */
+
    public void setJpeg(String j)
    {
       jpeg = j;
    }
+   /*
+   *@return  String  path to a .jpg file
+   */
    public String getJpeg()
    {
       return jpeg;
    }
+   /*
+   *@param  int v integer value of the card
+   */
    public void setValue(int v)
    {
       value = v;
    }
+   /*
+   *@return  int  integer value of the card
+   */
    public int getValue()
    {
       return value;
